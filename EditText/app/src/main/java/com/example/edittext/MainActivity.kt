123fca84e410ed6d4844622cacfd205784ef61a9
Jinseo <jinseopark94@gmail.com>
2021-01-01 20:22:07 +0900

    first commit

---
+++ EditText/app/src/main/java/com/example/edittext/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.edittext
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
