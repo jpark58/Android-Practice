474302eedc03aa81766bf934268bfc691267fe98
Jinseo <jinseopark94@gmail.com>
2021-01-05 21:42:51 +0900

    fragment practice

---
+++ Fragment/app/src/main/java/com/example/fragment/DetailFragment.kt
@@ -0,0 +1,42 @@
+package com.example.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_detail.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DetailFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DetailFragment : Fragment() {
+
+    var mainActivity:MainActivity? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater!!.inflate(R.layout.fragment_detail, container, false)
+        view.btnBack.setOnClickListener { mainActivity?.goBack() }
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+
+
+}
---
+++ Fragment/app/src/main/java/com/example/fragment/ListFragment.kt
@@ -0,0 +1,43 @@
+package com.example.fragment
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_list.*
+import kotlinx.android.synthetic.main.fragment_list.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ListFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ListFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    var mainActivity: MainActivity? = null
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater!!.inflate(R.layout.fragment_list, container, false)
+        view.btnNext.setOnClickListener{mainActivity?.goDetail()}
+        return view
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+
+}
